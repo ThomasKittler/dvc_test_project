acf402d65cd25210580efd43c22ce94afe089d33
Dr. Thomas Kittler <thomas.kittler@infoteam.de>
2023-10-09 17:16:14 +0200

    Add raw data

---
+++ src/get_data.py
@@ -0,0 +1,47 @@
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+
+
+def get_data():
+
+    digits = datasets.load_digits()
+
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        data,
+        digits.target,
+        test_size=0.5,
+        shuffle=False
+    )
+
+    np.savetxt(
+        fname="/home/thomas/projects/tutorials/dvc_own_test/data/train/X.csv",
+        X=X_train,
+        delimiter=";"
+    )
+
+    np.savetxt(
+        fname="/home/thomas/projects/tutorials/dvc_own_test/data/train/y.csv",
+        X=y_train,
+        delimiter=";"
+    )
+
+    np.savetxt(
+        fname="/home/thomas/projects/tutorials/dvc_own_test/data/test/x.csv",
+        X=X_test,
+        delimiter=";"
+    )
+
+    np.savetxt(
+        fname="/home/thomas/projects/tutorials/dvc_own_test/data/test/y.csv",
+        X=y_test,
+        delimiter=";"
+    )
+
+
+if __name__ == "__main__":
+    get_data()
